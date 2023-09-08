b96089b2a43813506261fc85fe4f221aaba4fff6
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-08 15:45:22 -0700

    Lower case alphabetical orders

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z'; k++);
+	putchar(k);
+	putchar('\n');
+
+	return (0);
+
+}
