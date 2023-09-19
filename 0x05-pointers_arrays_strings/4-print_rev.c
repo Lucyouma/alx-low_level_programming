be9dbeb533c5ef5778070e613fecfcf63d3b3a25
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-18 18:51:14 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - string prints in reverse mode
+ *
+ * @s: a reverse string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		putchar(*s);
+		s--;
+	}
+
+	putchar('\n');
+}
