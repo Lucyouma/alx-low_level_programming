50a3bd79487992b55421a197dfcc338095bb034c
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-25 13:14:51 -0700

    Print chaseboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_chessboard - Entry point
+  * chaseboard print
+  *
+  * @a: variable function declaration
+  *
+  * Return: 0 Always
+  */
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int r;
+
+	for (k = 0; k < 8; k++)
+	{
+	for (r = 0; r < 8; r++)
+	putchar(a[k][r]);
+	putchar('\n');
+		}
+		}
