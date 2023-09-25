142294af9acd4433e5ce5c9532eeaf39569a19c3
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-25 15:45:40 -0700

    Doing pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - Entry point
+  *
+  * @s: Function
+  *
+  * @to: Variable function
+  *
+  * Return: Always 0
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
