97e841320a3b947036d45164afe1ba0512001273
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-27 15:25:23 -0700

    String length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - String length return
+ *
+ * @s: Measured string
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
