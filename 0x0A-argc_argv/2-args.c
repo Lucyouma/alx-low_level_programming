7975b7044a83089f23fd8f0fe47039732f1c3735
Lucy Ouma <akinyilucy09@gmail.com>
2023-09-29 17:55:11 -0700

    Print received arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point, all arguments received
+ *
+ * @argc: arguments numbers
+ * @argv: arguments arrays
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
