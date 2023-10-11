00016b5b07b88eb5aba1927ffdfe33341e98fdee
Lucy Ouma <akinyilucy09@gmail.com>
2023-10-11 15:52:26 -0700

    Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,27 @@
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - Prints correct performed function
+ *
+ * @s: The argments passed op
+ *
+ * Return: Function pointer corresponding to the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,63 @@
+#include "3-calc.h"
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+/**
+ * op_add - Returns two numbers sum
+ * @a: Number one
+ * @b: Number two
+ *
+ * Return: The a and b sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns two numbers difference
+ * @a: Number one
+ * @b: Number two
+ *
+ * Return: The a and b difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns two numbers product
+ * @a: Number two
+ * @b: Number two
+ *
+ * Return: The a and b product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: Number one
+ * @b: Number two
+ *
+ * Return: The a and b quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns  two numbers divison remainder
+ * @a: Number one
+ * @b: Number two
+ *
+ * Return: The by b division remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
