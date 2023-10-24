ad251c980be4014d6980bd181a6248b07fdb4b37
Lucyouma <akinyilucy09@gmail.com>
2023-10-24 15:17:49 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
- listint_t *temp;
+	listint_t *temp;
 
- if (head == NULL)
- return;
+	if (head == NULL)
+	return;
 
- while (*head)
- {
- temp = (*head)->next;
- free(*head);
- *head = temp;
- }
-
- *head = NULL;
+	while (*head)
+	{
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	}
+	*head = NULL;
 }
