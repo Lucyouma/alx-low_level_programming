c09dec3c02c0ca69f3071f04aac7bb4815092edf
Lucyouma <akinyilucy09@gmail.com>
2023-10-24 18:12:04 -0700

    Listing safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,13 +16,13 @@ size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
-	
+
 	if (head == NULL || head->next == NULL)
 	return (0);
-	
+
 	tortoise = head->next;
 	hare = (head->next)->next;
-	
+
 	while (hare)
 	{
 	if (tortoise == hare)
@@ -33,25 +33,25 @@ size_t looped_listint_len(const listint_t *head)
 	nodes++;
 	tortoise = tortoise->next;
 	hare = hare->next;
- 	}
-	
+	}
+
 	tortoise = tortoise->next;
 	while (tortoise != hare)
 	{
 	nodes++;
 	tortoise = tortoise->next;
 	}
-	
+
 	return (nodes);
 	}
-	
+
 	tortoise = tortoise->next;
 	hare = (hare->next)->next;
 	}
-	
+
 	return (0);
 }
-	
+
 /**
  * print_listint_safe - A listint_t list safe printing.
  * @head: A listint_t list  pointer to the head
@@ -61,9 +61,9 @@ size_t looped_listint_len(const listint_t *head)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nodes, index = 0;
-	
+
 	nodes = looped_listint_len(head);
-	
+
 	if (nodes == 0)
 	{
 	for (; head != NULL; nodes++)
@@ -72,7 +72,7 @@ size_t print_listint_safe(const listint_t *head)
 	head = head->next;
 	}
 	}
-	
+
 	else
 	{
 	for (index = 0; index < nodes; index++)
@@ -80,9 +80,9 @@ size_t print_listint_safe(const listint_t *head)
 	printf("[%p] %d\n", (void *)head, head->n);
 	head = head->next;
 	}
-	
+
 	printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-	
+
 	return (nodes);
 }
