3ef5f65a630be4298a8c82377e0808a1d89770fa
Lucyouma <akinyilucy09@gmail.com>
2023-11-02 23:58:11 -0700

    This is my 0x15-file_io

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,11 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
